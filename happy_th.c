f84ed07f48bfc00cfb83adb6e31fed5d0f08403b
armiller <anthonymiller@outlook.com>
2013-11-21 14:08:53 -0800

    get opt done

--- happy_th.c
+++ happy_th.c
@@ -6,4 +6,38 @@
 #include<sys/types.h>
 #include<unistd.h>
 
+void usage(char *prog_name) 
+{
+	printf("%s [-p <number or procs/threads>] <number>\n", prog_name);
+	exit(EXIT_SUCCESS);
+}
 
+int main (int argc, char *argv[]) 
+{
+	int procs;
+	int opt;
+	int n;
+
+	if (argc < 2)
+		usage(argv[0]);
+
+	while ((opt = getopt(argc, argv, "p:h")) != -1) {
+		switch(opt) {
+			case 'p':
+				procs = atoi(optarg);
+				break;
+			case 'h':
+				usage(argv[0]);		
+				break;	
+			case '?':
+				exit(EXIT_FAILURE);
+		}
+	}
+
+	if (optind == argc) 
+		usage(argv[0]);
+
+	n = atoi(argv[optind]);
+
+	return 0;
+}
