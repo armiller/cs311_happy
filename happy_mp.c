93908ac86da850326c70bf4a70f655c1763dcfa8
armiller <anthonymiller@outlook.com>
2013-11-21 22:17:10 -0800

    integer bitmap creation

--- happy_mp.c
+++ happy_mp.c
@@ -14,8 +14,13 @@ int main (int argc, char *argv[])
 	int opt;
 	int n;
 	int i;
+	int *bitmap; 
 
 	n = get_args(argc, argv, &procs);
+
+	bitmap = malloc(((n/32) + 1) * sizeof(int));
+
+	bzero(bitmap, sizeof(bitmap));
 
 	return 0;
 }
