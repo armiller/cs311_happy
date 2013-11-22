a6301fe419382398ad9ac6c1a4ca89c5b29d71c6
armiller <anthonymiller@outlook.com>
2013-11-21 20:24:34 -0800

    Shared functions header

---
+++ functions.h
@@ -0,0 +1,18 @@
+/*
+ * Author: Anthony Miller
+ * Date: 11/21/2013
+ * Name: functions.h
+ *
+ * defines functions and adds default includes
+ */
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<getopt.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include"functions.c"
+
+void errEXIT(char *text);
+
+void usage(char *prog_name);
