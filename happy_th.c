6a17f6fa5641a34d121adf2d8cea6bd154d111a4
armiller <anthonymiller@outlook.com>
2013-11-23 21:15:33 -0800

    adding threads function

--- happy_th.c
+++ happy_th.c
@@ -7,15 +7,28 @@
  */
 
 #include"functions.h"
+#include<pthread.h>
 
 int main (int argc, char *argv[]) 
 {
 	int procs; 
+	int *bitmap;
 	int opt;
 	int n;
 	int i;
+	int s;
+	pthread_t *threads;
 
 	n = get_args(argc, argv, &procs);
+
+	threads = malloc(procs * sizeof(pthread_t));
+
+	bitmap = malloc(((n/32) + 1) *sizeof(int));
+
+	bzero(bitmap, (((n/32) + 1) * sizeof(int))); 
+
+	for (i = 0; i < procs; i++) {
+	}
 
 	return 0;
 }
